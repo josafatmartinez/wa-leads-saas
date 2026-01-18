0039071e05319f543562e3e06ee5e4cc23ff6ee8
Josafat Martinez <josafatmartinez@users.noreply.github.com>
2026-01-17 23:30:13 -0600

    🔧 chore: update frontend config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ["http://192.168.0.23"],
 };
 
 export default nextConfig;
